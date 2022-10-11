a1c97d3cb2766202638c6685666e61cc4f8f4229
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-10-11 23:12:33 +0530

    Added : Display Welcome to Snake and Ladder Simulation Program

---
+++ src/snake_ladder_simulation/SnakeAndLadderSimulation.java
@@ -0,0 +1,14 @@
+//This is a Snake and Ladder game simulation program.
+
+package snake_ladder_simulation;
+
+public class SnakeAndLadderSimulation {
+    SnakeAndLadderSimulation()
+    {
+        System.out.println("Welcome to Snake and Ladder Simulation \n");
+    }
+
+    public static void main(String[] args) {
+        SnakeAndLadderSimulation a = new SnakeAndLadderSimulation();
+    }
+}
